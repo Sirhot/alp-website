0c6922c3b4b9946e51ea2255289d2b562309230a
seko <serhatcubukcu@outlook.com>
2024-04-30 22:24:44 +0300

    navbar basics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import NavBar from "./components/NavBar";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <>
-      Portfolio
-    </>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
