2365f67f003e110b24f45c3cfc81b4b793179ac0
seko <serhatcubukcu@outlook.com>
2024-05-01 21:25:39 +0300

    styling navbar

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import "./styles.css";
 
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
