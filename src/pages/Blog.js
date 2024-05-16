c6de570a74b4b1bbf858603dbe7c9f0753a96df3
seko <serhatcubukcu@outlook.com>
2024-05-16 16:48:10 +0300

    portfolio-details

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,7 +1,25 @@
+import { clear } from '@testing-library/user-event/dist/clear'
 import React from 'react'
 
-export default function Blog() {
+export default function Blog({ value, handleSearchKey, clearSearch, formSubmit }) {
     return (
-        <div>Blog</div>
+        <div>
+            <section className='blog'>
+                <div className="container">
+                    <h1>Blog</h1>
+                    <div className="search-bar">
+                        <form onSubmit={formSubmit}>
+                            <input type="text"
+                                onChange={handleSearchKey}
+                                placeholder='Search By Category'
+                                value={value}
+                            />
+                            {value && <span onClick={clearSearch}>x</span>}
+                            <button>Go</button>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </div>
     )
 }
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,9 +1,22 @@
 import React from 'react'
-import project1 from '../images/project1.jpg'
-import project2 from '../images/project2.jpg'
-import project3 from '../images/project3.jpg'
+// if require method doesnt work: 
+//     dont forget to install file-loader and url-loader
+// npm install file-loader url-loader --save-dev
 
 export default function Portfolio() {
+
+    const [img, setImg] = React.useState("")
+    const images = ["project1", "project2", "project3"]
+
+    const clickHandler = (event) => {
+        images.forEach(element => {
+            if (element === event.target.name) {
+                setImg(element)
+            }
+        });
+
+    }
+
     return (
         <div>
             <section className="portfolio">
@@ -11,29 +24,37 @@ export default function Portfolio() {
                     <h1>Portfolio</h1>
                     <div className="projects">
                         <div className="project">
-                            <img src={project1} alt="project1" />
+                            <img src={require(`../images/project1.jpg`)} alt="project1" />
                             <h2>Work 1</h2>
                             <div className="project-btns">
-                                <a href="#">Details</a>
+                                <button type='button' name='project1' onClick={clickHandler}>Details</button>
                             </div>
                         </div>
                         <div className="project">
-                            <img src={project2} alt="project2" />
+                            <img src={require('../images/project2.jpg')} alt="project2" />
                             <h2>Work 2</h2>
                             <div className="project-btns">
-                                <a href="#">Details</a>
+                                <button type='button' name='project2' onClick={clickHandler}>Details</button>
                             </div>
                         </div>
                         <div className="project">
-                            <img src={project3} alt="project3" />
+                            <img src={require('../images/project3.jpg')} alt="project3" />
                             <h2>Work 3</h2>
                             <div className="project-btns">
-                                <a href="#">Details</a>
+                                <button type='button' name='project3' onClick={clickHandler}>Details</button>
                             </div>
                         </div>
                     </div>
                 </div>
-            </section>
-        </div>
+                {img &&
+                    <div id='focused-img'>
+                        <div id='details'>
+                            <img src={require(`../images/${img}.jpg`)} alt="dynamic"></img>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic alias, ducimus facere nihil est corporis. Blanditiis voluptatibus sint reiciendis quisquam, incidunt vitae quam voluptatum. Facere!</p>
+                        </div>
+                        <span onClick={() => (setImg(""))}>x</span>
+                    </div>}
+            </section >
+        </div >
     )
 }
